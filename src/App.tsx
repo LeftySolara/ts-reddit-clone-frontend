bc6e6a96b392c17361d52b783273adc7843a75b1
Julianne Adams <julianne@julianneadams.info>
2023-05-16 13:07:53 -0400

    Add: Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Button>Button</Button>
     </div>
   );
 }
---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  example: {
+    200: "#666666",
+    100: "#000000",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
